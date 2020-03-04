1bf98eedae069b6aaad29db25d598fd106a17619
Martin Kitsemägi <53197042+Martin-Kitsemagi@users.noreply.github.com>
2020-03-04 03:34:23 +0200

    Add files via upload

--- js/qualities.js
+++ js/qualities.js
@@ -8,9 +8,11 @@ function qualitiesSlider() {
         qualities_slides.is_mousedown = true;
 
         qualities_slides.x = event.clientX;
+        qualities_slides.y = event.clientY;
 
         if (event.type === "touchstart") {
             qualities_slides.x = event.touches[0].clientX;
+            qualities_slides.y = event.touches[0].clientY;
         }
 
         qualities_slides.slide_width = qualities_slides.outerWidth() / qualities_slides.find(".col-4").length;
@@ -23,40 +25,45 @@ function qualitiesSlider() {
     $(document).on("mousemove touchmove", function(event) {
         if (qualities_slides.hasClass("qualities_slides_active") && qualities_slides.is_mousedown === true) {
             var x = event.clientX;
+            var y = event.clientY;
 
             if (event.type === "touchmove") {
                 x = event.touches[0].clientX;
+                y = event.touches[0].clientY;
             }
 
-            var qualities_slides_position_left = qualities_slides.position().left;
-            var drag_speed = (x - qualities_slides.x) * 1.25;
+            if (Math.abs(y - qualities_slides.y) < 10) {
+                var qualities_slides_position_left = qualities_slides.position().left;
+                var drag_speed = (x - qualities_slides.x) * 1.25;
+                
+                if (drag_speed < 0) {
+                    if ((qualities_slides_position_left + qualities_slides.outerWidth() + drag_speed) > qualities_slides.slide_width) {
+                        qualities_slides.css("transform", "translate3d(" + (qualities_slides_position_left + drag_speed) + "px, 0, 0)");
+                    } else {
+                        qualities_slides.css("transform", "translate3d(" + (qualities_slides.slide_width - qualities_slides.outerWidth()) + "px, 0, 0)");
+                    }
+                } else if (drag_speed > 0) {
+                    if ((qualities_slides_position_left + drag_speed) < 0) {
+                        qualities_slides.css("transform", "translate3d(" + (qualities_slides_position_left + drag_speed) + "px, 0, 0)");
+                    } else {
+                        qualities_slides.css("transform", "translate3d(0, 0, 0)");
+                    }
+                }
+
+                var qualities_slides_scroll_index = Math.round(Math.abs(qualities_slides_position_left) / qualities_slides.slide_width);
+
+                if (qualities_slides_scroll_index !== qualities_slides.current_slide) {
+                    $(".qualities_slides_nav").removeClass("qualities_slides_nav_active");
+                    $(".qualities_slides_nav").eq(qualities_slides_scroll_index).addClass("qualities_slides_nav_active");
+
+                    qualities_slides.current_slide = qualities_slides_scroll_index;
+                };
+
+                qualities_slides.addClass("qualities_slides_scroll");
+            }
             
-            if (drag_speed < 0) {
-                if ((qualities_slides_position_left + qualities_slides.outerWidth() + drag_speed) > qualities_slides.slide_width) {
-                    qualities_slides.css("transform", "translate3d(" + (qualities_slides_position_left + drag_speed) + "px, 0, 0)");
-                } else {
-                    qualities_slides.css("transform", "translate3d(" + (qualities_slides.slide_width - qualities_slides.outerWidth()) + "px, 0, 0)");
-                }
-            } else if (drag_speed > 0) {
-                if ((qualities_slides_position_left + drag_speed) < 0) {
-                    qualities_slides.css("transform", "translate3d(" + (qualities_slides_position_left + drag_speed) + "px, 0, 0)");
-                } else {
-                    qualities_slides.css("transform", "translate3d(0, 0, 0)");
-                }
-            }
-
-            var qualities_slides_scroll_index = Math.round(Math.abs(qualities_slides_position_left) / qualities_slides.slide_width);
-
-            if (qualities_slides_scroll_index !== qualities_slides.current_slide) {
-                $(".qualities_slides_nav").removeClass("qualities_slides_nav_active");
-                $(".qualities_slides_nav").eq(qualities_slides_scroll_index).addClass("qualities_slides_nav_active");
-
-                qualities_slides.current_slide = qualities_slides_scroll_index;
-            };
-
             qualities_slides.x = x;
-
-            qualities_slides.addClass("qualities_slides_scroll");
+            qualities_slides.y = y;
         }
     });
 
